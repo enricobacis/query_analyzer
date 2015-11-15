c3d12588e058df6453560b75104509791fd55579
enricobacis <enrico.bacis@gmail.com>
2015-11-15 11:21:41 +0100

    Improve ParserNetwork class

--- src/parser/ParserNetwork.java
+++ src/parser/ParserNetwork.java
@@ -1,6 +1,7 @@
 package parser;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -12,7 +13,6 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-//basato su SAX
 public class ParserNetwork {
 
 	public static boolean node;
@@ -31,31 +31,23 @@ public class ParserNetwork {
 	public static boolean pailler;
 
 	private Node tmp_node;
-	private ArrayList<String> tmp_datas_enc;
-	private ArrayList<String> tmp_datas_plain;
+	private List<String> tmp_datas_enc;
+	private List<String> tmp_datas_plain;
 	private String tmp_nodeLinked;
 	private double tmp_latency;
 	private double tmp_throughput;
-	private ArrayList<Link> tmp_links;
+	private List<Link> tmp_links;
 	private String supportAttribute;
 
-	public ParserNetwork()
-	{
+	public ParserNetwork() {}
 
-	}
+	public List<Node> parseDocument(String res) {
+		final List<Node> output = new ArrayList<Node>();
 
-	public ArrayList<Node> parseDocument(String res)
-	{
-		final ArrayList<Node> output = new ArrayList<Node>();
-
-
-    try {
-
-
-
-		SAXParserFactory factory = SAXParserFactory.newInstance();
-		SAXParser saxParser = factory.newSAXParser();
-		DefaultHandler handler = new DefaultHandler() {
+		try {
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+			DefaultHandler handler = new DefaultHandler() {
 
 				public void startElement(String uri, String localName,String qName,
 			                Attributes attributes) throws SAXException {
@@ -63,68 +55,38 @@ public class ParserNetwork {
 					if (qName.equalsIgnoreCase("NODE")) {
 						node = true;
 						tmp_node = new Node();
-						String tmp_name = attributes.getValue("name");
-						tmp_node.setName(tmp_name);
-
-					}
-
-					if (qName.equalsIgnoreCase("NODE-TYPE")) {
+						tmp_node.setName(attributes.getValue("name"));
+					} else if (qName.equalsIgnoreCase("NODE-TYPE")) {
 						node_type = true;
-					}
-
-
-					if (qName.equalsIgnoreCase("NODE-DATAS")) {
+					} else if (qName.equalsIgnoreCase("NODE-DATAS")) {
 						node_datas = true;
 						tmp_datas_enc = new ArrayList<String>();
 						tmp_datas_plain = new ArrayList<String>();
-					}
-
-					if (qName.equalsIgnoreCase("NODE-DATA")) {
+					} else if (qName.equalsIgnoreCase("NODE-DATA")) {
 						supportAttribute = attributes.getValue("attribute");
 						node_data = true;
-					}
-
-					if (qName.equalsIgnoreCase("PERFORMANCE")) {
+					} else if (qName.equalsIgnoreCase("PERFORMANCE")) {
 						performance = true;
-					}
-
-					if (qName.equalsIgnoreCase("AES-THROUGHPUT")) {
+					} else if (qName.equalsIgnoreCase("AES-THROUGHPUT")) {
 						aes = true;
-					}
-
-					if (qName.equalsIgnoreCase("PAILLER-THROUGHPUT")) {
+					} else if (qName.equalsIgnoreCase("PAILLER-THROUGHPUT")) {
 						pailler = true;
-					}
-
-					if (qName.equalsIgnoreCase("BCLO-VALUE-TIME")) {
+					} else if (qName.equalsIgnoreCase("BCLO-VALUE-TIME")) {
 						bclo = true;
-					}
-
-					if (qName.equalsIgnoreCase("LINKS")) {
+					} else if (qName.equalsIgnoreCase("LINKS")) {
 						tmp_links = new ArrayList<Link>();
 						links = true;
-					}
-
-					if (qName.equalsIgnoreCase("LINK")) {
+					} else if (qName.equalsIgnoreCase("LINK")) {
 						link = true;
-					}
-
-					if (qName.equalsIgnoreCase("NODE-LINKED")) {
+					} else if (qName.equalsIgnoreCase("NODE-LINKED")) {
 						node_linked = true;
-					}
-
-					if (qName.equalsIgnoreCase("LATENCY")) {
+					} else if (qName.equalsIgnoreCase("LATENCY")) {
 						latency = true;
-					}
-
-					if (qName.equalsIgnoreCase("THROUGHPUT")) {
+					} else if (qName.equalsIgnoreCase("THROUGHPUT")) {
 						throughput = true;
-					}
-
-					if (qName.equalsIgnoreCase("COST-PER-SECOND")) {
+					} else if (qName.equalsIgnoreCase("COST-PER-SECOND")) {
 						cost_per_second = true;
 					}
-
 
 				}
 
@@ -134,24 +96,16 @@ public class ParserNetwork {
 					if (qName.equalsIgnoreCase("NODE")) {
 						output.add(tmp_node);
 						node = false;
-					}
-
-					if (qName.equalsIgnoreCase("NODE-DATAS")) {
+					} else if (qName.equalsIgnoreCase("NODE-DATAS")) {
 						tmp_node.setEncryptedVisibility(tmp_datas_enc);
 						tmp_node.setPlainVisibility(tmp_datas_plain);
-						node_datas= false;
-					}
-
-					if (qName.equalsIgnoreCase("PERFORMANCE")) {
+						node_datas = false;
+					} else if (qName.equalsIgnoreCase("PERFORMANCE")) {
 						performance = false;
-					}
-
-					if (qName.equalsIgnoreCase("LINKS")) {
+					} else if (qName.equalsIgnoreCase("LINKS")) {
 						tmp_node.setLinks(tmp_links);
 						links = false;
-					}
-
-					if (qName.equalsIgnoreCase("LINK")) {
+					} else if (qName.equalsIgnoreCase("LINK")) {
 						tmp_links.add(new Link(tmp_nodeLinked, tmp_latency, tmp_throughput));
 						link = false;
 					}
@@ -160,23 +114,24 @@ public class ParserNetwork {
 
 				public void characters(char ch[], int start, int length) throws SAXException {
 
-
 					if (node_type) {
 						String value = new String(ch, start, length);
 						tmp_node.setType(value);
 						node_type = false;
 					}
-
-
+					
 					if (node_data) {
-						String value = new String(ch, start, length);
-						if(value.equals("Encrypted"))
+						switch (new String(ch, start, length)) {
+						case "Encrypted":
 							tmp_datas_enc.add(supportAttribute);
-						if(value.equals("Plain"))
+							break;
+						
+						case "Plain":
 							tmp_datas_plain.add(supportAttribute);
-
-						// i "No" li conto come se non esistessero...
-
+							break;
+							
+						// The "No" are not taken into account.
+						}
 						node_data = false;
 					}
 
@@ -223,14 +178,13 @@ public class ParserNetwork {
 					}
 
 				}
-	     };
+			};
 
-	     saxParser.parse(res, handler);
+			saxParser.parse(res, handler);
 
-
-     } catch (Exception e) {
-       e.printStackTrace();
-     }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
     	return output;
    }
